3de69dd5c55f81a71ecf0bf00e827bb37f66fe74
Malcomn Mitchell <malcomn.j.mitchell@gmail.com>
2024-10-01 16:24:19 -0400

    save

--- src/main.jsx
+++ src/main.jsx
@@ -8,28 +8,39 @@ import { Provider } from "react-redux";
 import store from "./store";
 
 // core styles are required for all packages
-import '@mantine/core/styles.css';
-import '@mantine/notifications/styles.css';
+import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
 import { createTheme, MantineProvider } from "@mantine/core";
-import { ModalsProvider } from '@mantine/modals';
-import { Notifications } from '@mantine/notifications';
+import { ModalsProvider } from "@mantine/modals";
+import { Notifications } from "@mantine/notifications";
+import { QueryClientProvider, QueryClient} from "@tanstack/react-query";
 
-const theme = createTheme({
- 
+const theme = createTheme({});
+
+const TanstackQueryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchInterval: false,
+      refetchOnWindowFocus: false,
+    },
+  },
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.Fragment>
-    <MantineProvider theme={theme}>
-    <ModalsProvider>
-    <Notifications />
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
-      </ModalsProvider>
-    </MantineProvider>
+    <QueryClientProvider client={TanstackQueryClient}>
+      <MantineProvider theme={theme}>
+        <ModalsProvider>
+          <Notifications />
+          <Provider store={store}>
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
+          </Provider>
+        </ModalsProvider>
+      </MantineProvider>
+    </QueryClientProvider>
   </React.Fragment>
 );
 
