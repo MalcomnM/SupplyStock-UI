c81203196411d72926d6001227a5dfa9498b5f8f
Malcomn Mitchell <malcomn.j.mitchell@gmail.com>
2024-10-01 16:20:06 -0400

    save

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,10 @@ import store from "./store";
 
 // core styles are required for all packages
 import '@mantine/core/styles.css';
+import '@mantine/notifications/styles.css';
 import { createTheme, MantineProvider } from "@mantine/core";
+import { ModalsProvider } from '@mantine/modals';
+import { Notifications } from '@mantine/notifications';
 
 const theme = createTheme({
  
@@ -18,11 +21,14 @@ const theme = createTheme({
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.Fragment>
     <MantineProvider theme={theme}>
+    <ModalsProvider>
+    <Notifications />
       <Provider store={store}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
       </Provider>
+      </ModalsProvider>
     </MantineProvider>
   </React.Fragment>
 );
